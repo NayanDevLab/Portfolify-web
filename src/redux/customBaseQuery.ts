901250639fbabdb09c7169fa4bdc4fbbb7b7488f
nayanrdeveloper <nayanrdeveloper@gmail.com>
2025-06-26 22:33:12 +0530

    added backend route url

--- src/redux/customBaseQuery.ts
+++ src/redux/customBaseQuery.ts
@@ -29,7 +29,7 @@ type CustomBaseQueryArgs = {
 
 /* ---------- baseURL for Next ---------- */
 const API_URL =
-    process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/api/v1';
+    process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:4000/api/v1';
 
 const rawBaseQuery = fetchBaseQuery({
     baseUrl: API_URL,
